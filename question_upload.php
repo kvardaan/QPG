bc43debc248a713f49bba89bdc11064967dab85a
Vardaan Khattar <79071264+kvardaan@users.noreply.github.com>
2023-11-25 13:03:51 +0530

    code complete

--- question_upload.php
+++ question_upload.php
@@ -9,7 +9,7 @@
         <div class="row">
             <div class="col-md-8">
                 <h3><span style="font-size: 1.2em;">Upload</span><br>
-                    <span style="font-size: 1.5em; color: #ff8239; text-decoration: underline;;">Questions</span>
+                    <span style="font-size: 1.5em; color: #ff8239; text-decoration: underline; text-shadow: 1px 1px 2.5px;">Questions</span>
                 </h3>
             </div>
             <div class="col-md-4">
@@ -36,18 +36,31 @@
                     <select class="form-control" id="semester">
                     </select>
 
-                    <br><label class="my-1 mr-2" for="subject">Select Subject</label>
-                    <select class="form-control" id="subject">
-                    </select>
+                    <form action="include\upload.inc.php" method="post" enctype="multipart/form-data">
+                        <!-- Add a hidden input field for 'subject' -->
+                        <input type="hidden" name="subject" id="selectedSubject" value="">
 
-                    <br><form action="include\upload.inc.php" method="post" enctype="multipart/form-data">
-                        <label for="excel">Upload Questions(*excel files only)</label>
-                        <input type="file" name="excel" id="excel" accept=".xls, .xlsx, .csv" required>
+                        <!-- Modify the 'subject' dropdown -->
+                        <br>
+                        <label class="my-1 mr-2" for="subject">Select Subject</label>
+                        <select class="form-control" id="subject" name="subject" onchange="updateSelectedSubject()">
+                        </select>
+
+                        <br><label for="excel">Upload Questions(*excel files only)</label>
+                        <input style="margin-top: 1%;" type="file" name="excel" id="excel" accept=".xls, .xlsx, .csv" required>
                         <div style="text-align: center;">
                             <button type="submit" name="submit">Upload</button>
                         </div>
                     </form>
                     
+                    <?php
+                        if (isset($_GET['success']) && $_GET['success'] == 1) {
+                            echo <<<'HTML'
+                                <p class="success-blink" style="text-align: center; padding: .75%;">Success! Questions uploaded</p>
+                            HTML;
+                        }
+                    ?>
+
                 </div>
             </div>
         </div>
